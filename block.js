46cbeeb2b111e000015f4d243609d296a8754101
pallavi546 <74195532+pallavi546@users.noreply.github.com>
2021-03-14 18:35:09 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,22 +1,21 @@
-class Block{
-    constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world, this.body);
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
       }
-      display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        imageMode(CENTER);
-        rect(pos.x, pos.y, this.width, this.height);
-        pop();
-      }
-}
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x,pos.y, this.width, this.height);
+         }
+  };
